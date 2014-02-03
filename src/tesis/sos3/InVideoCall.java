9b62ac508f1d5772d2e57c70c24d4d109b445969
Francisco Alonso <francisco.a.alonso.s@gmail.com>
2014-02-02 21:42:14 -0430

    find out why session ID==null

--- src/tesis/sos3/InVideoCall.java
+++ src/tesis/sos3/InVideoCall.java
@@ -1,44 +1,53 @@
 package tesis.sos3;
 
 import org.doubango.ngn.sip.NgnAVSession;
+import org.doubango.ngn.utils.NgnStringUtils;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class InVideoCall extends Activity {
-
+	private static String TAG = InVideoCall.class.getCanonicalName();
+	
 	private RelativeLayout mMainLayout;
 	private View mViewInCallVideo;
 	private FrameLayout mViewLocalVideoPreview;
 	private FrameLayout mViewRemoteVideoPreview;
 	
 	private NgnAVSession mAVSession;
+	public String mid;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_in_video_call);
-		//mViewLocalVideoPreview = (FrameLayout)mViewInCallVideo.findViewById(R.id.view_call_incall_video_FrameLayout_local_video);
-		//mViewRemoteVideoPreview = (FrameLayout)mViewInCallVideo.findViewById(R.id.view_call_incall_video_FrameLayout_remote_video);
-		
-		//mMainLayout.removeAllViews();
-		//mMainLayout.addView(mViewInCallVideo);
-		//loadvideopreview from screenAV imsdroid
-		//mViewRemoteVideoPreview.removeAllViews();
-        final View remotePreview = mAVSession.startVideoConsumerPreview();
-		if(remotePreview != null){
+		mid = getIntent().getStringExtra("id");
+		Log.e(TAG, String.format("SESSION ID: %d", NgnStringUtils.parseLong(mid, -1)));
+		mAVSession = NgnAVSession.getSession(NgnStringUtils.parseLong(mid, -1));
+		if(mAVSession == null){
+			Log.e(TAG, String.format("SESSION ID if null: %d", NgnStringUtils.parseLong(mid, -1)));
+			Toast.makeText(this, "Session == NULL", Toast.LENGTH_SHORT).show();
+		}else{
+			Toast.makeText(this, "Session != NULL", Toast.LENGTH_SHORT).show();
+			mAVSession.incRef();
+			mAVSession.setContext(this);
+		}
+        //final View remotePreview = mAVSession.startVideoConsumerPreview();
+		/*if(remotePreview != null){
             final ViewParent viewParent = remotePreview.getParent();
             if(viewParent != null && viewParent instanceof ViewGroup){
                   ((ViewGroup)(viewParent)).removeView(remotePreview);
             }
             mViewRemoteVideoPreview.addView(remotePreview);
-        }
+        }*/
 	}
 
 	@Override
